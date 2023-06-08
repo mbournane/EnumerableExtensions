62cb78f77f4db5d79369e743b2ead3b75024677f
Bournane Mohamed <mbournane@gmail.com>
2023-06-08 05:15:18 +0200

    enumerable extension

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+namespace EnumerableExtensions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Veuillez saisir un entier: ");
+        int nb = Int32.Parse(Console.ReadLine());
+        int[] intTab = new int[nb];
+
+        for(int i = 0; i<= nb - 1;i++)
+        {
+            intTab[i] = i;
+        }
+
+        int sum = intTab.Aggregate((x, y) => x + y);
+
+        foreach(int n in intTab)
+        {
+            Console.Write(n + " ");
+        }
+
+        Console.WriteLine("La mayenne: " + sum/nb);
+
+    }
+}
